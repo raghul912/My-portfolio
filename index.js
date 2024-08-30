be50d171234591ee79479fd1b853d6fa2ce75635
Raghul T <82992066+raghul912@users.noreply.github.com>
2024-08-30 13:40:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let clg1 = document.getElementById('clg1');
+
+function toggle1(){
+    document.getElementById("menu").classList.toggle
+    ("active");
+
+}
+/*window.addEventListener('scroll',() => {
+    let value = window.scrollY;
+    clg1.style.marginLeft=value*1.5+'px';
+})*/
